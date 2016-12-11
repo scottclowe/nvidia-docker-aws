aa0df6e827821a8992b79469f9907c297663f3a1
Kevin Wilson <khwilson@gmail.com>
2016-12-11 12:14:59 -0500

    Add `set` commands to top of script
    It's always nice to have the `set` commands to die when necessary and to print the current instructions.

--- nvidia.sh
+++ nvidia.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -x
+set -e
+
 if [[ $EUID -ne 0 ]]; then
   # http://askubuntu.com/questions/15853/how-can-a-script-check-if-its-being-run-as-root
   echo "This script must be run as root"
