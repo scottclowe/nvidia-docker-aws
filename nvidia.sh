eaa680f29bfa54b734ced8c398c58669fefcedfe
Kevin Wilson <khwilson@gmail.com>
2016-12-13 22:04:48 -0500

    Forgot `deb` command in sources.list
    Whoops

--- nvidia.sh
+++ nvidia.sh
@@ -14,7 +14,7 @@ apt-get update && apt-get -y upgrade
 
 # Add the relevant docker repositories
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-echo "https://apt.dockerproject.org/repo ubuntu-xenial main" | tee /etc/apt/sources.list.d/docker.list
+echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | tee /etc/apt/sources.list.d/docker.list
 apt-get update
 apt-cache policy docker-engine
 
