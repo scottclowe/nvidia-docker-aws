014a1fdc8a4fd291b04f25dd17431fdcd1870799
Kevin Wilson <khwilson@gmail.com>
2016-12-13 22:27:17 -0500

    /var/log -> /var/lib
    Why are there so many subdirectories of /var that start with l?

--- nvidia.sh
+++ nvidia.sh
@@ -44,7 +44,7 @@ nvidia-docker run --rm nvidia/cuda nvidia-smi
 
 # Move the docker images to /mnt where there's a *lot* more space
 service docker stop
-mv /var/log/docker /mnt
-ln -s /mnt/docker /var/log/docker
+mv /var/lib/docker /mnt
+ln -s /mnt/docker /var/lib/docker
 service docker start
 nvidia-docker run --rm nvidia/cuda nvidia-smi
